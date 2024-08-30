9d7d8fd06c54880f038415648f8a6e06417c3731
Gary Orsargos <garman9813@gmail.com>
2024-08-30 19:50:07 -0400

    Added contact card functionality and demo page

---
+++ website/components/demo/cardDemo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+  </head>
+  <link rel='stylesheet' href="../styles/pageGrid.css">
+<?php
+// Define variables
+  $namearr = array("Joseph Smith", "Johnny Appleseed", "Oprah Winfrey", "Bill Belichek");
+  $numberarr = array("772-789-0987", "561-909-6547", "765-278-3827", "278-940-3513");
+  $emailarr = array("skibbidi@yahoo.com", "nfl@nba.com", "test123@google.edu", "fakeemail@com.com");
+  $companyarr = array("Mr. Beast Media", "PGA Tour", "Ultimate Fighting Championship", "McDonalds");
+  ?>
+  <link href="../styles/card.css" rel="stylesheet">
+
+  <body>
+  <div class = 'cardGrid'>
+    <?php
+    for($i = 0; $i < 4; $i++ ){      
+        $name = $namearr[$i];
+        $number = $numberarr[$i];
+        $email = $emailarr[$i];
+        $company = $companyarr[$i];
+        include '../contactCard.php'; 
+    }
+    ?>
+</div>
+</body>
+</html>
