eb4a397cf95216a2fb8177422d2ec2728da79f09
Gary Orsargos <garman9813@gmail.com>
2024-09-04 19:06:35 -0400

    Added search bar base component and demo inclusion

--- Frontend/components/demo/cardDemo.php
+++ Frontend/components/demo/cardDemo.php
@@ -7,15 +7,19 @@
   </head>
   <link rel='stylesheet' href="../styles/pageGrid.css">
 <?php
-// Define variables for test array
+  // Define variables for test array
   $namearr = array("Joseph Smith", "Johnny Appleseed", "Oprah Winfrey", "Bill Belichek");
   $numberarr = array("772-789-0987", "561-909-6547", "765-278-3827", "278-940-3513");
   $emailarr = array("skibbidi@yahoo.com", "nfl@nba.com", "test123@google.edu", "fakeemail@com.com");
   $companyarr = array("Mr. Beast Media", "PGA Tour", "Ultimate Fighting Championship", "McDonalds");
   ?>
   <link href="../styles/card.css" rel="stylesheet">
+  <link href="../styles/searchBar.css" rel="stylesheet">
 
   <body>
+    <?php
+      include '../searchBar.php';
+    ?>
   <div class = 'cardGrid'>
     <?php
     //For each example contact, reference the invidivuals components and inject the php card with those vars
