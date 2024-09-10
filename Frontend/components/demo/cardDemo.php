3aa839b46de2af91705cb3248128dc8aded2aba0
Gary Orsargos <garman9813@gmail.com>
2024-09-10 09:05:06 -0400

    Initial Tests for Querying db and inserting on contact page

--- Frontend/components/demo/cardDemo.php
+++ Frontend/components/demo/cardDemo.php
@@ -6,13 +6,6 @@
     <title><?php echo $pageTitle; ?></title>
   </head>
   <link rel='stylesheet' href="../styles/pageGrid.css">
-<?php
-  // Define variables for test array
-  $namearr = array("Joseph Smith", "Johnny Appleseed", "Oprah Winfrey", "Bill Belichek");
-  $numberarr = array("772-789-0987", "561-909-6547", "765-278-3827", "278-940-3513");
-  $emailarr = array("skibbidi@yahoo.com", "nfl@nba.com", "test123@google.edu", "fakeemail@com.com");
-  $companyarr = array("Mr. Beast Media", "PGA Tour", "Ultimate Fighting Championship", "McDonalds");
-  ?>
   <script src="https://kit.fontawesome.com/ac1c3ec324.js" crossorigin="anonymous"></script>
   <link href="../styles/card.css" rel="stylesheet">
   <link href="../styles/searchBar.css" rel="stylesheet">
@@ -26,8 +19,51 @@
       include '../searchBar.php';
     ?>
     <div class = 'cardGrid'>
-    <!--
-    <?php
+
+      <script>
+      $(".searchSubmitBtn").click(function(){
+        let nameVar = "Joseph Smith";
+        let numberVar = "67890";
+        let emailVar = "gary@gmail.com";
+        let companyVar = "UCF Knights";
+        let urlRequest = new URL("http://jo531962ucf.xyz/LAMPAPI/contacts/contacts.php");
+        
+        urlRequest.searchParams.append('req_type', 'read');
+        urlRequest.searchParams.append('user_id', 456);
+        urlRequest.searchParams.append('contact_id', 567);
+        
+        const options = {
+          method: 'GET',
+          mode: 'no-cors'
+        };
+
+        console.log(urlRequest.toString());
+
+        fetch(urlRequest, options)
+        .then((json) => console.log(json));
+
+        $.ajax({
+           url: '../contactCard.php',
+           method: 'GET',
+          success: function(response) {
+          response = response.replaceAll('*CONTACT_NAME*', nameVar)
+                             .replaceAll('*CONTACT_NUMBER*', numberVar)
+                             .replaceAll('*CONTACT_EMAIL*', emailVar)
+                             .replaceAll('*CONTACT_COMPANY*', companyVar);
+
+          $('.cardGrid').append(response);
+        }
+      }
+      )
+    });
+
+      </script>
+</div>
+
+
+
+<!--
+<?php
     //For each example contact, reference the invidivuals components and inject the php card with those vars
     for($i = 0; $i < 4; $i++ ){      
         $name = $namearr[$i];
@@ -64,29 +100,6 @@
 
     ?>
     -->
-      <script>
-      $(".searchSubmitBtn").click(function(){
-        let nameVar = "Joseph Smith";
-        let numberVar = "67890";
-        let emailVar = "gary@gmail.com";
-        let companyVar = "UCF Knights";
 
-        $.ajax({
-           url: '../contactCard.php',
-           method: 'GET',
-          success: function(response) {
-          response = response.replaceAll('*CONTACT_NAME*', nameVar)
-                             .replaceAll('*CONTACT_NUMBER*', numberVar)
-                             .replaceAll('*CONTACT_EMAIL*', emailVar)
-                             .replaceAll('*CONTACT_COMPANY*', companyVar);
-
-          $('.cardGrid').append(response);
-        }
-      }
-      )
-    });
-
-      </script>
-</div>
 </body>
 </html>
