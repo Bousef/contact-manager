e7450550401a9e87a1377e369f8a00767a777387
Gary Orsargos <123672165+garyorsargos@users.noreply.github.com>
2024-09-10 19:25:46 -0400

    Update cardDemo.php page title

--- Frontend/components/demo/cardDemo.php
+++ Frontend/components/demo/cardDemo.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $pageTitle; ?></title>
+    <title>Contact Manager</title>
   </head>
   <link rel='stylesheet' href="../styles/pageGrid.css">
   <script src="https://kit.fontawesome.com/ac1c3ec324.js" crossorigin="anonymous"></script>
