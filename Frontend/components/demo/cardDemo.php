35040e91cf12915b614184ee42403770257f2519
jluna2209 <83725494+jluna2209@users.noreply.github.com>
2024-09-18 16:16:49 -0400

    Update cardDemo.php

--- Frontend/components/demo/cardDemo.php
+++ Frontend/components/demo/cardDemo.php
@@ -105,7 +105,7 @@
     //Jose editing from this point
     
     function doEdit(contactId) {
-    window.location.href = `editContact.php?contact_id=${contactId}`;
+    window.location.href = `../editContact.php?contact_id=${contactId}`;
 
       }
 
