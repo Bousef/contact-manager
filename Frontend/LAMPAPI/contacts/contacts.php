7463236aa3cd55d57a499a757dce1cfc00fec480
jluna2209 <83725494+jluna2209@users.noreply.github.com>
2024-09-18 12:27:49 -0400

    Update contacts.php

--- Frontend/LAMPAPI/contacts/contacts.php
+++ Frontend/LAMPAPI/contacts/contacts.php
@@ -13,7 +13,7 @@
     require_once 'read_contacts_for_user.php';
     require_once 'update_contact_for_user.php';
     require_once 'delete_contact_for_user.php';
-    //require_once 'create_address_for_contact.php';
+    require_once 'create_address_for_contact.php';
 
     // Get the request data
     $json_req = file_get_contents('php://input');
@@ -42,38 +42,53 @@
         case 'create':
         {
 
-            // Ensure all necessary parameters are set (including parameters not yet handled by function)
-            if 
-            (
-                isset($json_decoded['user_id'])         &&
-                isset($json_decoded['first_name'])      &&
-                isset($json_decoded['last_name'])       &&
-                isset($json_decoded['phone_number'])    &&
-                isset($json_decoded['email']) &&
-                isset($json_decoded['address_line_01']) &&
-                isset($json_decoded['address_line_02']) &&
-                isset($json_decoded['city']) &&
-                isset($json_decoded['state']) &&
-                isset($json_decoded['zip_code'])
-                //&&
-               // isset($json_decoded['street_address'])
-            )
-            {
-                create_contact_for_user($json_decoded['user_id'], $json_decoded['first_name'],
-                $json_decoded['last_name'], $json_decoded['phone_number'],
-                $json_decoded['email'], $json_decoded['address_line_01'],
-                $json_decoded['address_line_02'], $json_decoded['city'],
-                $json_decoded['state'], $json_decoded['zip_code']);
-            }
-            else
-            {
-                send_error_response(ErrorCodes::MISSING_PARAMETERS);
-                return;
-            }
+           // Ensure all necessary parameters are set (including parameters not yet handled by function)
+        if 
+        (
+            isset($json_decoded['user_id'])         &&
+            isset($json_decoded['first_name'])      &&
+            isset($json_decoded['last_name'])       &&
+            isset($json_decoded['phone_number'])    &&
+            isset($json_decoded['email']) &&
+            isset($json_decoded['address_line_01']) &&
+            isset($json_decoded['address_line_02']) &&
+            isset($json_decoded['city']) &&
+            isset($json_decoded['state']) &&
+            isset($json_decoded['zip_code'])
+        )
+        {
+            // Create the contact and get the contact ID
+            $contact_id = create_contact_for_user(
+                $json_decoded['user_id'], 
+                $json_decoded['first_name'],
+                $json_decoded['last_name'], 
+                $json_decoded['phone_number'],
+                $json_decoded['email']
+            );
 
-            break;
+            if ($contact_id !== null) {
+                // Create the address for the contact
+                create_address_for_contact(
+                    $contact_id, 
+                    $json_decoded['address_line_01'],
+                    $json_decoded['address_line_02'], 
+                    $json_decoded['city'],
+                    $json_decoded['state'], 
+                    $json_decoded['zip_code']
+                );
 
+                // Send success response
+                echo json_encode(array("success" => true, "contact_id" => $contact_id));
+            }
         }
+        else
+        {
+            send_error_response(ErrorCodes::MISSING_PARAMETERS);
+            return;
+        }
+
+        break;
+    }
 
         case 'read':
         {
