84dabc3f46163bba12c0d9f137e1ad18de0bfe8c
Gary Orsargos <garman9813@gmail.com>
2024-09-19 21:04:43 -0400

    Bug Fixing

--- Frontend/components/navBar.php
+++ Frontend/components/navBar.php
@@ -2,11 +2,9 @@
   <h1 class="siteHeader">
     Contact Manager
   </h1>
-  <form action="/LAMPAPI/users/logout_user.php" method="post" style="display:inline;">
     <button type="submit" class="profileBtn" onclick="doLogout()">
       <i class="fa-solid fa-door-open icon profileIcon"></i>
     </button>
-  </form>
   <script>
     function doLogout() {
         sessionStorage.clear();
