5441545d5f6cfb5e9bba278e981e46c8155061c3
Gary Orsargos <garman9813@gmail.com>
2024-09-19 21:03:02 -0400

    Logout function

--- Frontend/components/navBar.php
+++ Frontend/components/navBar.php
@@ -3,8 +3,14 @@
     Contact Manager
   </h1>
   <form action="/LAMPAPI/users/logout_user.php" method="post" style="display:inline;">
-    <button type="submit" class="profileBtn">
+    <button type="submit" class="profileBtn" onclick="doLogout()">
       <i class="fa-solid fa-door-open icon profileIcon"></i>
     </button>
   </form>
+  <script>
+    function doLogout() {
+        sessionStorage.clear();
+        window.location.href = "https://jo531962ucf.xyz";
+    }
+    </script>
 </div>
