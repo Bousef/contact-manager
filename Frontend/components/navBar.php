7e9f487f0643407e5e57977078effaef89dffc77
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-20 22:44:11 -0400

    Update navBar.php

--- Frontend/components/navBar.php
+++ Frontend/components/navBar.php
@@ -3,16 +3,20 @@
     Contact Manager
   </h1>
   <div class = "navButtons">
-    <button type="submit" class="addContactBtn" onclick="window.location.href='https://jo531962ucf.xyz/contactsPage.php';" title="Home" aria-label="Home Button">
-      <i class="fa-solid fa-house icon profileIcon"></i>
+    <button type="submit" id = "buttonID" class="addContactBtn" onclick="window.location.href='https://jo531962ucf.xyz/contactsPage.php';" title="Add Contact" aria-label="Add Contact">
+      <i class="fa-solid fa-plus icon plusIcon"></i>
     </button>
     <button type="submit" class="profileBtn" onclick="doLogout()" title="Logout" aria-label="Logout Button">
       <i class="fa-solid fa-door-open icon profileIcon"></i>
     </button>
   </div>
   <script>
-    
-    
+    function flipButton() { // For flipping add contact button to home when not at home.
+      if(window.location.pathranme == "/addContacts.php") {
+        const buttonID = document.getElementById("buttonID");
+        buttonID.innerHTML = "<i class="fa-solid fa-house icon profileIcon"></i>";
+      }
+    }
     
     function doLogout() {
         sessionStorage.clear();
