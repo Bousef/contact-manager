b6ccbd99ad209b9b72213cde54600802bf7bbc74
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-20 23:27:11 -0400

    Update navBar.php

--- Frontend/components/navBar.php
+++ Frontend/components/navBar.php
@@ -3,7 +3,7 @@
     Contact Manager
   </h1>
   <div class = "navButtons">
-    <button type="submit" id = "buttonID" class="profileBtn addContactBtn" onclick="window.location.href='https://jo531962ucf.xyz/contactsPage.php';" title="Add Contact" aria-label="Add Contact">
+    <button type="submit" id = "buttonID" class="profileBtn addContactBtn" onclick="window.location.href='https://jo531962ucf.xyz/addContacts.php';" title="Add Contact" aria-label="Add Contact">
       <i class="fa-solid fa-plus icon plusIcon"></i>
     </button>
     <button type="submit" class="profileBtn" onclick="doLogout()" title="Logout" aria-label="Logout Button">
@@ -14,8 +14,10 @@
     function flipButton() { // For flipping add contact button to home when not at home.
       if(window.location.pathname == "/addContacts.php") {
         const buttonID = document.getElementById("buttonID");
+        buttonID.onclick = "window.location.href = 'https://jo531962ucf.xyz/contactsPage.php';";
         buttonID.innerHTML = "<i class='fa-solid fa-house icon profileIcon'></i>";
-
+        buttonID.setAttribute("title", "Home");
+        buttonID.setAttribute("aria-label", "Home");
         buttonID.classList.remove("addContactBtn");
       }
     }
