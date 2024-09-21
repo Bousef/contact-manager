e3ba9770c3bb5e39f3ef4e566109b3edad5f66d2
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-20 23:04:20 -0400

    Update searchBar.php to remove duplicate add contact button.

--- Frontend/components/searchBar.php
+++ Frontend/components/searchBar.php
@@ -3,7 +3,4 @@
   <button type="button" class="searchSubmitBtn" title="Search Contacts" aria-label="Search Contacts Button">
     <i class="fa-solid fa-magnifying-glass icon"></i>
   </button>
-  <button type="button" class="addContactBtn" title="Add Contact" aria-label="Add Contact">
-      <i class="fa-solid fa-plus icon plusIcon"></i>
-    </button>
 </div>
