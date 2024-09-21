019c68e938d6fa3ff6eaf3c2df8326cf6ab43e19
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-20 21:50:31 -0400

    Update navBar.php

--- Frontend/components/navBar.php
+++ Frontend/components/navBar.php
@@ -2,16 +2,18 @@
   <h1 class="siteHeader">
     Contact Manager
   </h1>
+  <div class = "navButtons">
     <button type="submit" class="profileBtn" onclick="window.location.href='https://jo531962ucf.xyz/contactsPage.php';" aria-label="Home Button">
       <i class="fa-solid fa-house icon profileIcon"></i>
     </button>
     <button type="submit" class="profileBtn" onclick="doLogout()" title="Logout" aria-label="Logout Button">
       <i class="fa-solid fa-door-open icon profileIcon"></i>
     </button>
+  </div>
   <script>
     function doLogout() {
         sessionStorage.clear();
         window.location.href = "https://jo531962ucf.xyz";
     }
-    </script>
+  </script>
 </div>
