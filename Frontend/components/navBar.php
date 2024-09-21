e768a910f0a83a28cce98f68ca30e51d57df2f8a
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-20 22:24:57 -0400

    Update navBar.php

--- Frontend/components/navBar.php
+++ Frontend/components/navBar.php
@@ -3,7 +3,7 @@
     Contact Manager
   </h1>
   <div class = "navButtons">
-    <button type="submit" class="profileBtn" onclick="window.location.href='https://jo531962ucf.xyz/contactsPage.php';" aria-label="Home Button">
+    <button type="submit" class="profileBtn" onclick="window.location.href='https://jo531962ucf.xyz/contactsPage.php';" title="Logout" aria-label="Home Button">
       <i class="fa-solid fa-house icon profileIcon"></i>
     </button>
     <button type="submit" class="profileBtn" onclick="doLogout()" title="Logout" aria-label="Logout Button">
