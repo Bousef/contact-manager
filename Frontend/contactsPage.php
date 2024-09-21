ff97a611a8a37cfe08bfd05f3052bee6fa212d5d
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-20 20:34:54 -0400

    Updated contactsPage.php to add favicon

--- Frontend/contactsPage.php
+++ Frontend/contactsPage.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Manager</title>
+    <title>Contacts</title>
   </head>
   <link rel='stylesheet' href="components/styles/pageGrid.css">
   <script src="https://kit.fontawesome.com/ac1c3ec324.js" crossorigin="anonymous"></script>
