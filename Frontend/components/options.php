e49598629e12c2f774074b39ec26c77a82e69f77
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-22 00:49:36 -0400

    Update options.php

--- Frontend/components/options.php
+++ Frontend/components/options.php
@@ -2,22 +2,17 @@
   <div class = "overlay" id = "overlayID" onclick = "closeOptions()"></div>
 
   <div class = "options" id = "optionsID">
-    <h1>popup test</h1>
+    <h1>
+      Options
+    </h1>
+    <div class = "logoutOption">
+      <p>Logout</p>
+      <button type="submit" class="profileBtn logoutBtn" onclick="doLogout()" title="Logout" aria-label="Logout Button">
+        <i class="fa-solid fa-gear icon profileIcon"></i>
+      </button>
+    </div>
   </div>
 
-<!--
-  <h1 class="siteHeader">
-    Contact Manager
-  </h1>
-  <div class = "navButtons">
-    <button type="submit" id = "buttonID" class="profileBtn addContactBtn" onclick="window.location.href='https://jo531962ucf.xyz/addContacts.php';" title="Add Contact" aria-label="Add Contact">
-      <i class="fa-solid fa-plus icon plusIcon"></i>
-    </button>
-    <button type="submit" class="profileBtn logoutBtn" onclick="doLogout()" title="Logout" aria-label="Logout Button">
-      <i class="fa-solid fa-gear icon profileIcon"></i>
-    </button>
-  </div>
--->
 
   <script>
     function doLogout() {
