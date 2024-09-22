156444d633031823c642cd9a568f22ed6b1865be
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-21 22:43:02 -0400

    Update navBar.php

--- Frontend/components/navBar.php
+++ Frontend/components/navBar.php
@@ -7,7 +7,7 @@
       <i class="fa-solid fa-plus icon plusIcon"></i>
     </button>
     <button type="submit" class="profileBtn logoutBtn" onclick="doLogout()" title="Logout" aria-label="Logout Button">
-      <i class="fa-solid fa-door-open icon profileIcon"></i>
+      <i class="fa-solid fa-cog icon profileIcon"></i>
     </button>
   </div>
   <script>
