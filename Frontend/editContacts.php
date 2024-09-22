56455e83ad3544e69a0a9ff2ffeb3fdac3e9d373
Gary Orsargos <123672165+garyorsargos@users.noreply.github.com>
2024-09-22 19:47:48 -0400

    Update editContacts.php

--- Frontend/editContacts.php
+++ Frontend/editContacts.php
@@ -9,11 +9,11 @@
     
     <meta charset="UTF-8">
     <title>Contacts</title>
-    <link href="../../css/style.css" rel="stylesheet">
-    <link href="../styles/navBar.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="components/styles/navBar.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/ac1c3ec324.js" crossorigin="anonymous"></script>
-    <link href="../styles/card.css" rel="stylesheet">
-    <link href="../styles/options.css" rel="stylesheet">
+    <link href="components/styles/card.css" rel="stylesheet">
+    <link href="components/styles/options.css" rel="stylesheet">
 </head>
 <body id="body">
     <?php include 'components/navBar.php'; ?>
