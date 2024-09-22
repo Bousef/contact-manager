1c71ae87beb4c5e208f9040fed2c2001f7dc7a91
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-21 21:45:41 -0400

    Update addContacts.php

--- Frontend/addContacts.php
+++ Frontend/addContacts.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contacts</title>
     
+    
     <link href="css/style.css" rel="stylesheet">
     <link href="components/styles/navBar.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/ac1c3ec324.js" crossorigin="anonymous"></script>
