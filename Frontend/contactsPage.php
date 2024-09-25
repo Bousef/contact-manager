6a32b0813216cccdb2973805f284a4585423b74b
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-09-25 19:02:51 -0400

    Update contactsPage.php

--- Frontend/contactsPage.php
+++ Frontend/contactsPage.php
@@ -70,7 +70,7 @@ if (document.documentElement.scrollHeight > window.innerHeight) {
             });
 
             function scrollCards() {
-                if($(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
+                if($(window).scrollTop() + $(window).height() >= $(document).height() - 1000) {
                     createGrid(false);
                 }
             }
