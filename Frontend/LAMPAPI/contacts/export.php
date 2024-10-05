8233009dee36c274191f180eaf462725d0bb7cf2
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-10-05 19:51:09 -0400

    Update export.php

--- Frontend/LAMPAPI/contacts/export.php
+++ Frontend/LAMPAPI/contacts/export.php
@@ -1,3 +1,9 @@
 <?php
+  //read_contacts_for_user($json_decoded['user_id'], $json_decoded['search_string'], $json_decoded['limit'], $json_decoded['offset']);
+  $vcf_file = "test_work_pls";
 
+  // Send to user.
+  header('Content-Type: text/vcard');
+  header('Content-Disposition: attachment; filename="contacts.vcf"');
+  echo $vcf_content;
 ?>
