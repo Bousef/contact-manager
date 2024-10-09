64e984546a9b96a73f89da7b08ef883894df2c4b
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-10-09 11:28:49 -0400

    Update export.php

--- Frontend/LAMPAPI/contacts/export.php
+++ Frontend/LAMPAPI/contacts/export.php
@@ -1,8 +1,8 @@
 <?php
-  $contacts = read_contacts_for_user($json_decoded['user_id'], $json_decoded['search_string'], $json_decoded['limit'], $json_decoded['offset']);
+  /*$contacts =*/ read_contacts_for_user($json_decoded['user_id'], $json_decoded['search_string'], $json_decoded['limit'], $json_decoded['offset']);
   $vcf_file = "";
 
-  foreach($contacts.result as $contact) {
+  //foreach($contacts.result as $contact) {
     //$address = 
     
     $vcf_file .= "BEGIN:VCARD\n";
@@ -12,7 +12,7 @@
     
     $vcf_file .= "END:VCARD\n";
     $vcf_file .= "\n";
-  }
+  //}
 
   // Send to user.
   header('Content-Type: text/vcard');
