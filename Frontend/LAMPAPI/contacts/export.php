9f0a6e334dc70bd2762fe7b20f18ef3ab773ba73
Kyle Cahalan <100001157+ThatCodingKMC@users.noreply.github.com>
2024-10-11 22:56:31 -0400

    Update export.php

--- Frontend/LAMPAPI/contacts/export.php
+++ Frontend/LAMPAPI/contacts/export.php
@@ -2,11 +2,20 @@
   ob_start();
   read_contacts_for_user($json_decoded['user_id'], $json_decoded['search_string'], $json_decoded['limit'], $json_decoded['offset']);
   $contacts = json_decode(ob_get_clean(), true);
-  //$temp1 = ob_get_clean();
-  //$temp2 = json_decode($temp1, true);
+
+  // Find address
+  foreach($contacts["result"] as $contact) {
+    require_once 'addresses/read_address_for_contact.php';
+    read_address_for_contact($contact['id']);
+    $address = json_decode(ob_get_clean(), true);
+    if($address['success'] != true) {
+      $contact['address'] = false;
+    } else {
+      $contact['address'] = $address['result'];
+    }
+  }
 
   $vcf_file = "";
-
   foreach($contacts["result"] as $contact) {
     
     $vcf_file .= "BEGIN:VCARD\n";
@@ -16,12 +25,7 @@
     if($contact['last_name'] || $contact['first_name']) $vcf_file .= "FN:{$contact['first_name']} {$contact['last_name']}\n";
     if($contact['phone_number']) $vcf_file .= "TEL;CELL:{$contact['phone_number']}\n";
     if($contact['email_address']) $vcf_file .= "EMAIL;HOME:{$contact['email_address']}\n";
-
-    // Addresses
-    //require_once 'addresses/read_address_for_contact.php';
-    //read_address_for_contact($contact['id']);
-    //$address = json_decode(ob_get_clean(), true);
-    //if($address) $vcf_file .= "ADR;TYPE=HOME:;{$address["result"]['address_line_02']};{$address["result"]['address_line_01']};{$address["result"]['city']};{$address["result"]['state']};{$address["result"]['zip_code']}\n";
+    if($contact['address']) $vcf_file .= "ADR;TYPE=HOME:;{$contact['address']['address_line_02']};{$contact['address']['address_line_01']};{$contact['address']['city']};{$contact['address']['state']};{$contact['address']['zip_code']}\n";
     
     $vcf_file .= "END:VCARD\n";
     $vcf_file .= "\n";
